729f25e988c0a780016b11a238404c5849448c67
Sonjoy barman <sonjoybarmon19@gmail.com>
2025-08-16 20:42:46 +0600

    test file modify

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,15 +1,11 @@
-const request = require("supertest");
-
-const { expect } = require("chai");
-
-const app = require("../app");
+import request from "supertest";
+import { expect } from "chai";
+import app from "../app.js";
 
 describe("GET /", () => {
   it('should respond with "Hello, World!"', async () => {
     const response = await request(app).get("/");
-
     expect(response.statusCode).to.equal(200);
-
     expect(response.text).to.equal("Hello, World!");
   });
 });
